c03f89eb64f23ae4c4d39b5113be7d0107163361
Daniel Hermel <danielhermel.oliveira@gmail.com>
2024-12-23 16:38:44 -0300

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -3,4 +3,5 @@ git commit -m "Ultimo Commit"
 git push --set-upstream origin twitter-main
 go build main.go
 del main.zip
-tar -a cf main.zip main
+tar -cf main.tar main
+Compress-Archive -Path main.tar -DestinationPath main.zip
